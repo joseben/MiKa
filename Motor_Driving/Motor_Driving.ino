7a6c8fbe5b68014bc9926c0252c5cb3bbc2e3570
Jose Ben <36511657+joseben@users.noreply.github.com>
2022-10-07 13:37:56 +0530

    Add files via upload

---
+++ Motor_Driving/Motor_Driving.ino
@@ -0,0 +1,79 @@
+#define pwm1 2
+#define pwm2 4
+#define pwm3 6
+#define pwm4 8
+#define dir1 3
+#define dir2 5
+#define dir3 7
+#define dir4 9
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pwm1,OUTPUT);
+  pinMode(pwm2,OUTPUT);
+  pinMode(pwm3,OUTPUT);
+  pinMode(pwm4,OUTPUT);
+  pinMode(dir1,OUTPUT);
+  pinMode(dir2,OUTPUT);
+  pinMode(dir3,OUTPUT);
+  pinMode(dir4,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
+
+void forward(){
+  digitalWrite(dir1,HIGH);
+  digitalWrite(dir2,HIGH);
+  digitalWrite(dir3,HIGH);
+  digitalWrite(dir4,HIGH);
+  analogWrite(pwm1,50);
+  analogWrite(pwm2,50);
+  analogWrite(pwm3,50);
+  analogWrite(pwm4,50);
+}
+
+void backward(){
+  digitalWrite(dir1,LOW);
+  digitalWrite(dir2,LOW);
+  digitalWrite(dir3,LOW);
+  digitalWrite(dir4,LOW);
+  analogWrite(pwm1,50);
+  analogWrite(pwm2,50);
+  analogWrite(pwm3,50);
+  analogWrite(pwm4,50);
+}
+void right(){
+  digitalWrite(dir1,HIGH);
+  digitalWrite(dir2,HIGH);
+  digitalWrite(dir3,LOW);
+  digitalWrite(dir4, LOW);
+  analogWrite(pwm1,50);
+  analogWrite(pwm2,50);
+  analogWrite(pwm3,50);
+  analogWrite(pwm4,50);
+}
+void left(){
+  digitalWrite(dir1,LOW);
+  digitalWrite(dir2,LOW);
+  digitalWrite(dir3,HIGH);
+  digitalWrite(dir4,HIGH);
+  analogWrite(pwm1,50);
+  analogWrite(pwm2,50);
+  analogWrite(pwm3,50);
+  analogWrite(pwm4,50);
+}
+void stop(){
+  int i=50;
+    for(i-50;i>=0;i--){
+      analogWrite(pwm1,i);
+      analogWrite(pwm2,i);
+      analogWrite(pwm3,i);
+      analogWrite(pwm4,i);
+      delay(20);
+      }
+}
